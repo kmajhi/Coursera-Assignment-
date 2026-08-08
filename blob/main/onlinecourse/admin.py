42a517ee795ffbd1e1569d5d58e71257f5335bee
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2026-08-08 18:32:16 +0600

    Add admin configuration for course management

---
+++ blob/main/onlinecourse/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['question_text', 'grade']
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    inlines = [QuestionInline]
+
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
