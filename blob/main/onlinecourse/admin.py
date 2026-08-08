93b90983300de0cbf38dd8c6f11e77d467fe2e14
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2026-08-08 19:46:04 +0600

    Update admin configurations for Course and Lesson models

--- blob/main/onlinecourse/admin.py
+++ blob/main/onlinecourse/admin.py
@@ -1,26 +1,47 @@
 from django.contrib import admin
 from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
+
 class ChoiceInline(admin.StackedInline):
     model = Choice
-    extra = 2
+    extra = 4
+
 
 class QuestionInline(admin.StackedInline):
     model = Question
-    extra = 2
+    extra = 1
+
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 1
+
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
-    list_display = ['question_text', 'grade']
+    list_display = ('question_text', 'lesson', 'grade')
+    list_filter = ['lesson']
+    search_fields = ['question_text']
+
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
     inlines = [QuestionInline]
+    list_display = ('title', 'course', 'order')
+    list_filter = ['course']
+    search_fields = ['title']
 
-admin.site.register(Course)
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ('name', 'pub_date', 'total_enrollment')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
-admin.site.register(Instructor)
-admin.site.register(Learner)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
+admin.site.register(Instructor)
+admin.site.register(Learner)
 admin.site.register(Submission)
