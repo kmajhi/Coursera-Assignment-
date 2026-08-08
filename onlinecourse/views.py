60fc21c37c48a8b4697c1c05608d94cba691315b
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2026-08-08 20:37:00 +0600

    Refactor registration and exam submission logic

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,3 +1,105 @@
+import logging
+
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import Course, Enrollment, Question, Choice, Submission
+
+logger = logging.getLogger(__name__)
+
+
+def registration_request(request):
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'onlinecourse/user_registration_bootstrap.html', context)
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except User.DoesNotExist:
+            logger.error("New user")
+        if not user_exist:
+            user = User.objects.create_user(
+                username=username, first_name=first_name,
+                last_name=last_name, password=password
+            )
+            login(request, user)
+            return redirect("onlinecourse:index")
+        else:
+            context['message'] = "User already exists."
+            return render(request, 'onlinecourse/user_registration_bootstrap.html', context)
+
+
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('onlinecourse:index')
+        else:
+            context['message'] = "Invalid username or password."
+            return render(request, 'onlinecourse/user_login_bootstrap.html', context)
+    else:
+        return render(request, 'onlinecourse/user_login_bootstrap.html', context)
+
+
+def logout_request(request):
+    logout(request)
+    return redirect('onlinecourse:index')
+
+
+class CourseListView(generic.ListView):
+    template_name = 'onlinecourse/course_list_bootstrap.html'
+    context_object_name = 'course_list'
+
+    def get_queryset(self):
+        user = self.request.user
+        courses = Course.objects.order_by('-total_enrollment')[:10]
+        for course in courses:
+            if user.is_authenticated:
+                course.is_enrolled = Enrollment.objects.filter(user=user, course=course).exists()
+        return courses
+
+
+class CourseDetailView(generic.DetailView):
+    model = Course
+    template_name = 'onlinecourse/course_details_bootstrap.html'
+
+
+def enroll(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    user = request.user
+
+    is_enrolled = Enrollment.objects.filter(user=user, course=course).exists()
+    if not is_enrolled and user.is_authenticated:
+        Enrollment.objects.create(user=user, course=course, mode='honor')
+        course.total_enrollment += 1
+        course.save()
+
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:course_details', args=(course.id,)))
+
+
+def extract_answers(request):
+    submitted_answers = []
+    for key in request.POST:
+        if key.startswith('choice'):
+            choice_id = int(request.POST[key])
+            submitted_answers.append(choice_id)
+    return submitted_answers
+
+
 def submit(request, course_id):
     course = get_object_or_404(Course, pk=course_id)
     user = request.user
@@ -14,15 +116,6 @@ def submit(request, course_id):
     return HttpResponseRedirect(
         reverse('onlinecourse:show_exam_result', args=(course.id, submission.id))
     )
-
-
-def extract_answers(request):
-    submitted_answers = []
-    for key in request.POST:
-        if key.startswith('choice'):
-            choice_id = int(request.POST[key])
-            submitted_answers.append(choice_id)
-    return submitted_answers
 
 
 def show_exam_result(request, course_id, submission_id):
@@ -42,6 +135,7 @@ def show_exam_result(request, course_id, submission_id):
 
     context = {
         'course': course,
+        'submission': submission,
         'grade': total_score,
         'total_score': total_score,
         'possible_score': possible_score,
